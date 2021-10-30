c8731c9466f10c310708dbd727525cc7d931c980
Melanie Rodas <mrodasl@uni.pe>
2021-10-29 19:44:56 -0500

    added link color - anchor ccs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,2 @@
-// nabvar responsive
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+
+
