4e85c5e125526708af97f956ab4f521b35539bd1
Melanie Rodas <mrodasl@uni.pe>
2021-10-30 21:39:53 -0500

    added mobile menu

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
+const mainMenu = document.querySelector('.navbar__item');
+const closeMenu = document.querySelector('.close_menu');
+const openMenu = document.querySelector('.open_menu');
 
+openMenu.addEventListener('click', openNav);
+closeMenu.addEventListener('click', closeNav);
 
+function openNav() {
+
+    document.getElementById("myNav").style.width = "100%";
+    
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
